cb5e68e0a7f6e8d9a7e5a3527baa8f75141da213
3572453qq <3572453@qq.com>
2024-02-18 08:18:53 +0800

    sql file for teams and computers

---
+++ lims_teamcomputer.sql
@@ -0,0 +1,15 @@
+/*
+SQLyog Enterprise - MySQL GUI
+MySQL - 8.0.32 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+insert into `lims_teamcomputer` (`id`, `teamid`, `computer_name`) values('1','3','LAB6F-01');
+insert into `lims_teamcomputer` (`id`, `teamid`, `computer_name`) values('2','1','LAB5F-01');
+insert into `lims_teamcomputer` (`id`, `teamid`, `computer_name`) values('3','1','LAB5F-02');
+insert into `lims_teamcomputer` (`id`, `teamid`, `computer_name`) values('4','2','LAB7F-01');
+insert into `lims_teamcomputer` (`id`, `teamid`, `computer_name`) values('5','2','LAB7F-02');
+insert into `lims_teamcomputer` (`id`, `teamid`, `computer_name`) values('6','4','uat-lab5f02');
+insert into `lims_teamcomputer` (`id`, `teamid`, `computer_name`) values('7','4','uat-lab5f01');
+insert into `lims_teamcomputer` (`id`, `teamid`, `computer_name`) values('8','4','uat-lab5f03');
---
+++ lims_teams.sql
@@ -0,0 +1,11 @@
+/*
+SQLyog Enterprise - MySQL GUI
+MySQL - 8.0.32 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+insert into `lims_teams` (`id`, `teamname`) values('1','研发一组');
+insert into `lims_teams` (`id`, `teamname`) values('2','研发二组');
+insert into `lims_teams` (`id`, `teamname`) values('3','研发三组');
+insert into `lims_teams` (`id`, `teamname`) values('4','测试中心');
